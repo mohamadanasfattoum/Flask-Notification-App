586d92dd14c0afd342d1f642171221a6cf44cf8c
mohamadanasfattoum <samaanas30@gmail.com>
2025-04-11 18:41:55 +0200

    run flask as parameter in SocketIo

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+socketio = SocketIO(app) # run flask as parameter in SocketIo 
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+
+if __name__=='__main__':
+    socketio.run(app,debug=True)
--- bfehele.md
+++ bfehele.md
@@ -1,3 +1,5 @@
 pip install flask flask-socketio
 
 source ../bin/activate
+
+pip freeze > requirement.txt
